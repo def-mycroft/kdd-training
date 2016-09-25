11f6678b4fec00781d2380bec43b338a98fd72fe
def-mycroft23 <dasenbrj@ksu.edu>
2016-09-25 01:42:27 -0500

    after doing some hacking around

--- solution.py
+++ solution.py
@@ -7,23 +7,39 @@ Rules:
 
 """
 
-print_string(string, factor):
+def print_string(string, factor):
     """ Prints "strint" a "factor" number of times"""
-    print string * factor
+    print (string + "\n") * factor
 
-#biggest(parameter1, parameter2):
-#    # takes two arguments and returns the larger of the two.
-#
-#mirror(integer):
-#    # returns a mirrored integer, reverse order.
-#
+def biggest(parameter1, parameter2):
+    """Returns the larger of two arguments"""
+    x = parameter1 if parameter1 > parameter2 else parameter2
+    return x
+
+
+def mirror(integer):
+    """ Returns the mirrored value of the input"""
+    for item in integer: print item
+    """
+    I don't know how to do this one. Something with iterating through the 
+    integer. I don't know how to do that. 
+    """
+
+def manyargs(*arg):
+    print "I was called with", len(arg), "arguments:", arg
+    print arg[1]
+
+manyargs(1,2,3,4,5)
+
 #swap_last_first(integer):
 #    # interchanges the first and the last digits.
 #
 #is_palindrome(number):
 #    # returns true if a number is a palindrome.
 #
-#factorial(number): # calculates the factorial of a number.  sum_list(list): # returns the sum of all numbers in a list.
+#factorial(number): # calculates the factorial of a number.  
+
+#sum_list(list): # returns the sum of all numbers in a list.
 #
 #
 #smallest():
@@ -37,8 +53,7 @@ print_string(string, factor):
 #make_odds(SIZE, START):
 #    # makes a list of odd numbers of size SIZE, starting with number START.  
 #
-
-fib(n):
+#fib(n):
     # calculates the nth fibionacii number.
 
 
