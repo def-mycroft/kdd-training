913bf54045a23d533d29809ea5d00d8f31a535b6
def-mycroft23 <dasenbrj@ksu.edu>
2016-09-25 01:14:27 -0500

    initial commit

---
+++ solution.py
@@ -0,0 +1,44 @@
+"""Solution to HW3
+
+Rules:
+
+    *Can only use one line of code in each function.
+    *Cannot use functions like min() or max() or any library functions.
+
+"""
+
+print_string(string, factor):
+    """ Prints "strint" a "factor" number of times"""
+    print string * factor
+
+#biggest(parameter1, parameter2):
+#    # takes two arguments and returns the larger of the two.
+#
+#mirror(integer):
+#    # returns a mirrored integer, reverse order.
+#
+#swap_last_first(integer):
+#    # interchanges the first and the last digits.
+#
+#is_palindrome(number):
+#    # returns true if a number is a palindrome.
+#
+#factorial(number): # calculates the factorial of a number.  sum_list(list): # returns the sum of all numbers in a list.
+#
+#
+#smallest():
+#    # takes an unlimited number of arguments and returns the smallest.
+#
+#
+#union(list1, list2):
+#    # takes two lists and returns the list that takes unique values from both.
+#
+#
+#make_odds(SIZE, START):
+#    # makes a list of odd numbers of size SIZE, starting with number START.  
+#
+
+fib(n):
+    # calculates the nth fibionacii number.
+
+
