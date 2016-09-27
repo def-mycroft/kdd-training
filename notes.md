335f4a6432d1c76ec74da61c9f751bcb2ad6e37c
def-mycroft23 <dasenbrj@ksu.edu>
2016-09-26 22:49:11 -0500

    after hacking around with this solution

---
+++ notes.md
@@ -0,0 +1,66 @@
+
+Rules for this homework:
+
+* Can only use one line of code in the body of each function.
+* Cannnot use functions like min() or max() or any library functions.
+
+#Topics for this Homework:
+
+## map
+
+The map() function takes a function and applies it to every member of an input
+list:
+
+    map(function, list_of_inputs)
+
+## filter
+
+The filter() function returns a list of values from an input list for which 
+a function returns True.
+
+    map(function, list_of_inputs)
+
+The filter and map function are similar concepts. The map function applies a 
+function to each member of a list and returns the output as a list, while the
+filter function applies a boolean function to each member of a list and returns
+the values (from the input list) for which the boolean function returns True.
+
+## reduce
+
+Reduce passes the current value of a list with the next value of the same list.
+
+In this manner:
+
+    output = reduce(function(x,y), list)
+
+Therefore, for example, a formula for the product of a list of numbers would 
+look like this:
+
+    factorial = reduce(lambda x,y: x*y, list)
+
+Note that unlike map and filter, the reduce function returns a single number.
+The map and filter functions return a list of numbers.
+
+## slicing tools
+## list/dict comprehension
+
+
+
+## lambdas
+
+A lambda is a short function that is not named. 
+
+Lambdas are used in the form:
+
+    lambda para1, para2: [function of para1 and para2]
+
+For example: 
+
+    labmda x,y: x + y
+
+
+This is useful for creating small, throwaway functions which are made up on
+the spot.
+
+## recursion
+
--- solution.py
+++ solution.py
@@ -1,15 +1,9 @@
-"""Solution to HW3
-
-Rules:
-
-    *Can only use one line of code in each function.
-    *Cannot use functions like min() or max() or any library functions.
-
-"""
+"""Solution to HW3"""
 
 def print_string(string, factor):
-    """ Prints "strint" a "factor" number of times"""
+    """ Prints "string" a "factor" number of times"""
     print (string + "\n") * factor
+
 
 def biggest(parameter1, parameter2):
     """Returns the larger of two arguments"""
@@ -19,17 +13,9 @@ def biggest(parameter1, parameter2):
 
 def mirror(integer):
     """ Returns the mirrored value of the input"""
-    for item in integer: print item
-    """
-    I don't know how to do this one. Something with iterating through the 
-    integer. I don't know how to do that. 
-    """
+    return mirrored
 
-def manyargs(*arg):
-    print "I was called with", len(arg), "arguments:", arg
-    print arg[1]
 
-manyargs(1,2,3,4,5)
 
 #swap_last_first(integer):
 #    # interchanges the first and the last digits.
@@ -37,22 +23,41 @@ manyargs(1,2,3,4,5)
 #is_palindrome(number):
 #    # returns true if a number is a palindrome.
 #
-#factorial(number): # calculates the factorial of a number.  
 
-#sum_list(list): # returns the sum of all numbers in a list.
-#
-#
-#smallest():
-#    # takes an unlimited number of arguments and returns the smallest.
-#
-#
-#union(list1, list2):
-#    # takes two lists and returns the list that takes unique values from both.
-#
-#
-#make_odds(SIZE, START):
-#    # makes a list of odd numbers of size SIZE, starting with number START.  
-#
+def factorial(number):
+    """ Calculates the factorial of a number."""
+    output = reduce(lambda x,y: x*y, range(1,(number+1)))
+    return output
+
+
+def sum_list(input_list):
+    """Returns the sum of all numbers in a list."""
+    output = reduce(lambda x,y: x + y, input_list)
+    return output
+
+
+def smallest():
+    """takes an unlimited number of arguments and returns the smallest."""
+    return output
+
+
+def union(list1, list2):
+    """Returns the list that contains unique values which are in both"""
+    output = list(filter(lambda x: x in list2, list1))
+    return output
+
+
+def make_odds(SIZE, START):
+    """Makes a SIZE length list of odd numbers, starting with START"""
+    output = list(filter(lambda x: x % 2 != 0, range(START, 100000)))
+    return range(output[0], output[SIZE + 1])
+
+
+while True:
+    size = int(raw_input("size>>"))
+    start = int(raw_input("start>>"))
+    print(make_odds(size, start))
+
 #fib(n):
     # calculates the nth fibionacii number.
 
